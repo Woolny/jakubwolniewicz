3fd9db4c72180a185118b681de5d18fe0bd90beb
Woolny <119405031+Woolny@users.noreply.github.com>
2024-03-24 23:41:44 +0100

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,24 @@
+import HeadeSection from "./components/HeaderSection";
+import HelloSection from "./components/HelloSection";
+import HeroSection from "./components/HeroSection";
+import ProjectSection from "./components/ProjectSection";
+import Footer from "./components/Footer";
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-white text-black">
+      <HeadeSection />
+      <div className="container mx-auto px-12 ">
+        <HelloSection />
+      </div>
+      <div>
+        <HeroSection />
+      </div>
+      <div>
+        <ProjectSection />
+      </div>
+      <Footer />
+    </main>
+  );
+}
